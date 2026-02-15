2d8f074a10759f2fd01f82736c138917a7828652
dkel <dkelll@users.noreply.github.com>
2026-02-15 01:36:43 -0600

    feat: init

---
+++ src/client/java/zap/ZapClient.java
@@ -0,0 +1,10 @@
+package zap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZapClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
