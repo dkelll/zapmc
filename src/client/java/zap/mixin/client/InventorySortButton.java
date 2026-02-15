16fd3efe6fe478ad8bdf75fb291c92a711551809
dkel <dkelll@users.noreply.github.com>
2026-02-15 17:07:41 -0600

    feat: Progress

---
+++ src/client/java/zap/mixin/client/InventorySortButton.java
@@ -0,0 +1,39 @@
+package zap.mixin.client;
+
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InventoryScreen.class)
+public abstract class InventorySortButton extends HandledScreen<PlayerScreenHandler> {
+
+    private InventorySortButton() {
+        super(null, null, null);
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void addSortButton(CallbackInfo ci) {
+        int x = this.x + this.backgroundWidth - 30;
+        int y = this.y + 10;
+
+            ButtonWidget sortButton = ButtonWidget.builder(Text.literal("S"), button -> {
+                onSortClicked();
+            })
+            .dimensions(x, y, 20, 20)
+            .build();
+
+        this.addDrawableChild(sortButton);
+    }
+
+    private void onSortClicked() {
+        System.out.println("Sort button clicked!");
+        // Sorting logic will go here
+    }
+}
+
